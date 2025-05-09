e5fbd4a35328eebed96f35528d9994838a8b9919
Arthur Gomes <153852894+mercuriohg@users.noreply.github.com>
2025-05-09 20:36:20 -0300

    Add files via upload

---
+++ Manipulação de dados/inicio.js
@@ -0,0 +1,7 @@
+var a = 5 + 3
+var b = a % 5
+var c = 5 * b ** 2
+var d = 10 - a / 2
+var e = 6 * 2 / d 
+var f = ++a
+console.log(a, b, c, d , e, f)
---
+++ Manipulação de dados/testetipos.js
@@ -0,0 +1,8 @@
+a = 5
+b = '5'
+c = a == b
+d = a === b
+idade = 17
+a2 = idade >= 18 ? "Maior":"Menor"
+console.log(a2)
+console.log(c, d)
