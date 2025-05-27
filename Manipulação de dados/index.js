a2f127604851a4467a7e6bf16b553b775818a03b
Arthur Gomes <153852894+mercuriohg@users.noreply.github.com>
2025-05-26 21:32:23 -0300

    Add files via upload

---
+++ Manipulação de dados/index.js
@@ -0,0 +1,8 @@
+let pais = 'Brasil'
+console.log(`Seu país é ${pais}`)
+if (pais != 'Brasil'){ //Condição Simples
+    console.log('Você é estrangeiro')
+
+}else{
+    console.log("Você é brasileiro")
+}
